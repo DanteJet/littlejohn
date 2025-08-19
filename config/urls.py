98b1876cc2e219384a613b94beebf098bd55ca78
Yakov Bubnov <y.bubnov@vktem.ru>
2025-08-20 00:46:11 +0800

    redirect logout to home page

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from core.views import CustomLoginView, CustomPasswordChangeView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='admin_dashboard'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
     path('', include('core.urls')),
